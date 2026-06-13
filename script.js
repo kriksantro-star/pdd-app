da5eb93686acce9c3f498a1f0bf09b443e7c3cb4
kriksantro-star <kriksantro@gmail.com>
2026-06-14 01:34:50 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.blue').addEventListener('click', function() {
+    alert('Открываю список билетов АВ...');
+});
+
+document.querySelector('.red').addEventListener('click', function() {
+    alert('Начинаем экзамен!');
+});
